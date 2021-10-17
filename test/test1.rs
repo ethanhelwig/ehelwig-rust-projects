cc644e75a900ed67dcd7dd8dc71099d8e5a429ab
ethanhelwig <ethanahelwig@gmail.com>
2021-10-16 21:58:41 -0400

    first commit

---
+++ test/test1.rs
@@ -0,0 +1,24 @@
+fn first_word(s: &String) -> &str{
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for(i, &item) in bytes.iter().enumerate(){
+        
+    }
+}
+
+fn main(){
+    let s = String::from("hello world");
+    let word = first_word(&s);
+    println!("{}", word)
+}
